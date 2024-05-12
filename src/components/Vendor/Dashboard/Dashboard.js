be19563272a684e025aecb97e672cff513b4e4e6
VAIBHAV GARG <63347618+Vaibhav2801@users.noreply.github.com>
2024-05-12 16:00:35 +0530

    Code Refactring

--- src/components/Vendor/Dashboard/Dashboard.js
+++ src/components/Vendor/Dashboard/Dashboard.js
@@ -16,12 +16,10 @@ import {
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import digitalFactory from './digitalFactory.js';
 import { AppBar, Drawer, DrawerHeader } from './Drawer.tsx';
 import { VENDOR_DRAWER_LIST } from '../../../utils/constants';
-import { Card, Col, Row, Steps, Form, Input, Button, Menu, Dropdown } from 'antd';
 import './Dashboard.css';
-import { DownOutlined } from "@ant-design/icons";
+import DigitalFactory from './DigitalFactory.js'
 
 
 const Dashboard = () => {
@@ -42,177 +40,6 @@ const Dashboard = () => {
     setTab(e.target.key);
   };
 
-
-
-
-
-
-  const [selectedOption, setSelectedOption] = useState(null);
-  const [buttonLabels, setButtonLabels] = useState(['Basic Details', 'Addresses & Contacts', 'C', 'D']); // Initial button labels
-
-  const forms = [
-    {
-      label: 'Form for A',
-      content: (
-        <Form
-      layout="vertical"
-    >
-      <Row gutter={16}>
-        <Col span={24}>
-          <Row gutter={16} align="middle">
-            <Col span={8}>
-              <Form.Item label="Company Logo">
-                <div className="company-logo">
-                  <img src="https://via.placeholder.com/150" alt="Company Logo" />
-                </div>
-              </Form.Item>
-            </Col>
-            <Col span={16}>
-              <Form.Item label="About Us" name="aboutUs">
-                <Input.TextArea rows={4}  placeholder='Provide your company description and an overview of your company'/>
-              </Form.Item>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-      <Row gutter={16}>
-        <Col span={12}>
-          <Form.Item
-            label="Field 1"
-            name="field1"
-            rules={[{ required: true, message: 'Field 1 is required' }]}
-          >
-            <Input className="custom-input" variant="filled"/>
-          </Form.Item>
-          <Form.Item
-            label="Field 2"
-            name="field2"
-            rules={[{ required: true, message: 'Field 2 is required' }]}
-          >
-            <Input className="custom-input" variant="filled"/>
-          </Form.Item>
-          <Form.Item
-            label="Field 3"
-            name="field3"
-            rules={[{ required: true, message: 'Field 3 is required' }]}
-          >
-            <Input className="custom-input" variant="filled"/>
-          </Form.Item>
-        </Col>
-        <Col span={12}>
-          <Form.Item
-            label="Field 4"
-            name="field4"
-            rules={[{ required: true, message: 'Field 4 is required' }]}
-          >
-            <Input className="custom-input" variant="filled"/>
-          </Form.Item>
-          <Form.Item
-            label="Field 5"
-            name="field5"
-            rules={[{ required: true, message: 'Field 5 is required' }]}
-          >
-            <Input className="custom-input" variant="filled"/>
-          </Form.Item>
-          <Form.Item
-            label="Field 6"
-            name="field6"
-            rules={[{ required: true, message: 'Field 6 is required' }]}
-          >
-            <Input className="custom-input" variant="filled"/>
-          </Form.Item>
-        </Col>
-      </Row>
-      
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
-      )
-    },
-    {
-      label: 'Form for B',
-      content: (
-        <Form>
-          <Form.Item label="Field B">
-            <Input />
-          </Form.Item>
-          {/* Add more form fields as needed */}
-        </Form>
-      )
-    },
-    {
-      label: 'Form for C',
-      content: (
-        <Form>
-          <Form.Item label="Field C">
-            <Input />
-          </Form.Item>
-          {/* Add more form fields as needed */}
-        </Form>
-      )
-    },
-    // Add more form configurations as needed
-  ];
-
-  const handleOptionChange = (index) => {
-    setSelectedOption(index);
-  };
-
-  const renderForm = () => {
-    if (selectedOption !== null && forms[selectedOption]) {
-      return forms[selectedOption].content;
-    }
-    return null;
-  };
-
-  const handleButtonNameChange = () => {
-    // Example function to change button labels
-    setButtonLabels(['E', 'F', 'G', 'H']); // Change button labels to E, F, G, H
-  };
-
-
-
-  const [current, setCurrent] = useState(0);
-  const onChange = (value) => {
-    console.log('onChange:', value);
-    setCurrent(value);
-  };
-
-
-  const [currentSub, setCurrentSub] = useState(0);
-  const onChangeSub = (value) => {
-    console.log('onChangeSub:', value);
-    setCurrentSub(value);
-  };
-  const description = 'This is a description.';
-
-  const { Step } = Steps;
-  
-
-  const StepDropdown = () => {
-    return (
-      <Steps
-      progressDot
-      direction='vertical'
-      current={currentSub}
-        onChange={onChangeSub}
-      items={[
-        {
-          title: 'Finished'
-        },
-        {
-          title: 'In Progress'
-        },
-        {
-          title: 'Waiting'
-        },
-      ]}
-    />
-    );
-  };
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -273,51 +100,7 @@ const Dashboard = () => {
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }} style={{ backgroundColor: '#f0f2f5' }}>
         <DrawerHeader />
-
-        <div>
-      <Row gutter={16}>
-        <Col span={6}>
-          <Card title="Card title" bordered hoverable style={{ height: '39rem' }}>
-            {/* Button to change button names */}
-            <Button onClick={handleButtonNameChange}>Change Button Names</Button>
-
-            <div>
-            
-
-      <Divider />
-
-      <Steps direction="vertical" current={current}
-        onChange={onChange}>
-      <Step title='Step 1' description={<StepDropdown />} />
-      <Step title="In Progress" description="This is a description." />
-      <Step title="Waiting" description="This is a description." />
-    </Steps>
-            </div>
-
-          </Card>
-        </Col>
-        <Col span={18}>
-          <Card bordered hoverable style={{ height: '39rem', overflow: 'auto', scrollbarWidth: 'thin'}}>
-            <div>
-              <h2 style={{ marginTop: '0' }}>Company Overview</h2>
-              <hr />
-              <Row style={{ justifyContent: 'center', alignItems: 'center', marginBottom: '16px' }}>
-                {/* Render buttons dynamically */}
-                {buttonLabels.map((label, index) => (
-                  <Button key={index} type={selectedOption == index ? 'primary' : 'default'} onClick={() => handleOptionChange(index)}>
-                    {label}
-                  </Button>
-                ))}
-              </Row>
-              <div>
-              {renderForm()}
-              </div>
-            </div>
-          </Card>
-        </Col>
-      </Row>
-    </div>
-
+          <DigitalFactory/>
       </Box>
     </Box>
   );
--- src/components/Vendor/Dashboard/digitalFactory.js
+++ src/components/Vendor/Dashboard/digitalFactory.js
@@ -1,11 +1,115 @@
-import React from 'react'
+import  { useState } from 'react';
+import { Divider,} from '@mui/material';
+import { Card, Col, Row, Steps, Button } from 'antd';
+import './Dashboard.css';
+import { forms } from '../CompanyDetails/CompanyDetails.js';
 
-const digitalFactory = () => {
+const { Step } = Steps;
+
+const DigitalFactory = () => {
+
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [buttonLabels, setButtonLabels] = useState(['Basic Details', 'Addresses & Contacts', 'C', 'D']); // Initial button labels
+  const [current, setCurrent] = useState(0);
+  const [currentSub, setCurrentSub] = useState(0);
+
+
+
+  const handleOptionChange = (index) => {
+    setSelectedOption(index);
+  };
+
+  const renderForm = () => {
+    if (selectedOption !== null && forms[selectedOption]) {
+      return forms[selectedOption].content;
+    }
+    return null;
+  };
+
+  const handleButtonNameChange = () => {
+    setButtonLabels(['E', 'F', 'G', 'H']); 
+  };
+
+  const onChange = (value) => {
+    console.log('onChange:', value);
+    setCurrent(value);
+  };
+
+  const onChangeSub = (value) => {
+    console.log('onChangeSub:', value);
+    setCurrentSub(value);
+  };
+
+  const StepDropdown = () => {
+    return (
+      <Steps
+      progressDot
+      direction='vertical'
+      current={currentSub}
+        onChange={onChangeSub}
+      items={[
+        {
+          title: 'Finished'
+        },
+        {
+          title: 'In Progress'
+        },
+        {
+          title: 'Waiting'
+        },
+      ]}
+    />
+    );
+  };
+
   return (
-    <div>digitalFactory
-    </div>
+   
+    <div>
+    <Row gutter={16}>
+      <Col span={6}>
+        <Card title="Card title" bordered hoverable style={{ height: '39rem' }}>
+          {/* Button to change button names */}
+          <Button onClick={handleButtonNameChange}>Change Button Names</Button>
+
+          <div>
+          
+
+    <Divider />
+
+    <Steps direction="vertical" current={current}
+      onChange={onChange}>
+    <Step title='Step 1' description={<StepDropdown />} />
+    <Step title="In Progress" description="This is a description." />
+    <Step title="Waiting" description="This is a description." />
+  </Steps>
+          </div>
+
+        </Card>
+      </Col>
+      <Col span={18}>
+        <Card bordered hoverable style={{ height: '39rem', overflow: 'auto', scrollbarWidth: 'thin'}}>
+          <div>
+            <h2 style={{ marginTop: '0' }}>Company Overview</h2>
+            <hr />
+            <Row style={{ justifyContent: 'center', alignItems: 'center', marginBottom: '16px' }}>
+              {/* Render buttons dynamically */}
+              {buttonLabels.map((label, index) => (
+                <Button key={index} type={selectedOption == index ? 'primary' : 'default'} onClick={() => handleOptionChange(index)}>
+                  {label}
+                </Button>
+              ))}
+            </Row>
+            <div>
+            {renderForm()}
+            </div>
+          </div>
+        </Card>
+      </Col>
+    </Row>
+  </div>
+
     
   )
 }
 
-export default digitalFactory
+export default DigitalFactory

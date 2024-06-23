33e8467d6cbad0594b98eaa298c979a72f240803
VAIBHAV GARG <63347618+Vaibhav2801@users.noreply.github.com>
2024-06-23 20:06:43 +0530

    Images Uploaded successfullt

--- src/components/ImageUpload/ImageUpload.js
+++ src/components/ImageUpload/ImageUpload.js
@@ -1,69 +1,61 @@
-import React, { useState, useEffect, useMemo } from 'react'
-import { Upload, Carousel, Flex, Image, Button, Card } from 'antd';
+import React, { useState, useEffect } from 'react'
+import { Upload, Image } from 'antd';
 import { COMPANY_ID } from '../../utils/constants';
 import { getPlantImages, uploadPlantImages } from '../../apis/Vendor/CompanyDetails';
 import { convertBufferToBinary } from '../../utils/helper';
 import { notification } from 'antd';
-import del from './../../assets/del.png'
+
+import { PlusOutlined } from '@ant-design/icons';
+import { MESSAGES } from '../../utils/locale';
 
 
-const Context = React.createContext({
-    name: 'Default',
-});
+const getBase64 = (file) =>
+    new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = (error) => reject(error);
+    });
+
+
+
 
 const ImageUpload = () => {
 
     const [fileList, setFileList] = useState([]);
-    const [srcList, setSrcList] = useState([]);
-    const [currentImage, setcurrentImage] = useState(null);
-    const [api, contextHolder] = notification.useNotification();
+    const [api] = notification.useNotification();
 
-    const deleteFile = []
 
-    // const [currentImage, setCurrentImage] = useState
+    const [previewOpen, setPreviewOpen] = useState(false);
+    const [previewImage, setPreviewImage] = useState('');
 
-    const openNotification = (placement) => {
+
+    const openNotification = (placement, message) => {
         api.success({
             message: `Success`,
-            description: <Context.Consumer>{({ name }) => `Machine Added Successfully`}</Context.Consumer>,
+            description: message,
             placement,
         });
     };
-    let contextValue = useMemo(
-        () => ({
-            name: 'Make Mate',
-        }),
-        [],
-    );
 
-    const openFailedNotification = (placement) => {
+
+    const openFailedNotification = (placement, message) => {
         api.error({
             message: `Something went wrong`,
-            description: <Context.Consumer>{({ name }) => `Unable to add Machine `}</Context.Consumer>,
+            description: message,
             placement,
         });
     };
-    contextValue = useMemo(
-        () => ({
-            name: 'Make Mate',
-        }),
-        [],
-    );
 
 
-    const deleteNotification = (placement) => {
+    const deleteNotification = (placement, message) => {
         api.success({
             message: `Success`,
-            description: <Context.Consumer>{({ name }) => `Machine Details deleted Successfully`}</Context.Consumer>,
+            description: message,
             placement,
         });
     };
-    contextValue = useMemo(
-        () => ({
-            name: 'Make Mate',
-        }),
-        [],
-    );
+
 
     const fetchImages = async () => {
         try {
@@ -74,18 +66,19 @@ const ImageUpload = () => {
                     let newSrcList = [];
                     res.data.company_Images.map(async (item, i) => {
                         let data = {
-                            name: "Image_"+i,
-                            src: convertBufferToBinary(item.image),
-                            type: 'image/png',
-                            id: i + 1
+                            uid: item._id,
+                            name: item.name,
+                            status: 'done',
+                            url: convertBufferToBinary(item.image),
                         }
                         newSrcList.push(data)
                     })
-                    setSrcList(newSrcList);
+                    setFileList(newSrcList);
                 }
             }
         }
         catch (err) {
+            openFailedNotification('topRight', MESSAGES.FETCH_IMAGE_ERROR)
             return err
         }
     }
@@ -102,154 +95,99 @@ const ImageUpload = () => {
     };
 
 
-    const handleImageSrc = () => {
-        try {
+    const uploadImage = async options => {
 
-            console.log(fileList)
-            const newSrcList = fileList.map((item) => ({
-                name: item.name,
-                src: URL.createObjectURL(item.originFileObj),
-                type: item.type,
-                id: item.uid,
-            }));
-            setSrcList([...srcList,newSrcList]);
-            setcurrentImage(srcList[0])
-        } catch (error) {
-            console.error("Error reading file:", error);
-        }
-    };
-
-
-    useEffect(() => {
-        handleImageSrc();
-    }, [fileList]);
-
-
-    useEffect(() => {
-        if (!currentImage) {
-            setcurrentImage(srcList[0]);
-        }
-    }, [srcList]);
-
-
-    const draggerProps = {
-        name: "file",
-        multiple: true,
-        showUploadList: false,
-        onChange: onChange,
-        fileList: fileList,
-        beforeUpload: false,
-    };
-
-    const uploadFiles = async () => {
+        const { onSuccess, onError, file, onProgress } = options;
         try {
             const res = await uploadPlantImages(COMPANY_ID, fileList)
             if (res.success) {
-                openNotification('topRight');
+                openNotification('topRight', MESSAGES.UPLOAD_PLANT_IMAGES);
                 fetchImages()
             }
+            onSuccess("Ok");
+            console.log("server res: ", res);
+        } catch (err) {
+            openFailedNotification('topRight', MESSAGES.UPLOAD_IMAGE_ERROR)
+            console.log("Eroor: ", err);
+            const error = new Error("Some error");
         }
-        catch (err) {
-            console.log(err)
-        }
-    }
+    };
 
-    console.log(fileList)
 
-    const handleCurrentImage = (item) => {
-        setcurrentImage(item)
-    }
-
-    const handleDelete = (item) => {
-        console.log(item)
-
-        const files =  srcList.filter((file) => {
-            if(file.name!=item.name)
-                {
-                    return file
-                }
+    const handleDelete = async (file) => {
+        const filesList = fileList.filter((item) => {
+            return item.uid == file.uid
         })
 
-        setFileList(files)
-        deleteFile.push(item.name.split('_')[1])
+        try {
+            const res = await uploadPlantImages(COMPANY_ID, filesList)
+            if (res.success) {
+                deleteNotification('topRight', MESSAGES.DELETE_PLANT_IMAGES);
+                fetchImages()
+            }
+            console.log("server res: ", res);
+        } catch (err) {
+            console.log("Eroor: ", err);
+            const error = new Error("Some error");
+        }
     }
 
-    
+    const handlePreview = async (file) => {
+        if (!file.url && !file.preview) {
+            file.preview = await getBase64(file.originFileObj);
+        }
+        setPreviewImage(file.url || (file.preview));
+        setPreviewOpen(true);
+    };
 
+
+    const uploadButton = (
+        <button
+            style={{
+                border: 0,
+                background: 'none',
+            }}
+            type="button"
+        >
+            <PlusOutlined />
+            <div
+                style={{
+                    marginTop: 8,
+                }}
+            >
+                Upload
+            </div>
+        </button>
+    );
     return (
         <div>
-            <Context.Provider value={contextValue}>
-                {contextHolder}
-                <Flex gap={50}>
+            <Upload
+                listType="picture-card"
+                fileList={fileList}
+                onPreview={handlePreview}
+                customRequest={uploadImage}
+                onChange={onChange}
+                onRemove={(file) => {
+                    handleDelete(file)
+                }}
+            >
+                {fileList.length >= 8 ? null : uploadButton}
+            </Upload>
+            {previewImage && (
+                <Image
+                    wrapperStyle={{ display: 'none' }}
+                    preview={{
+                        visible: previewOpen,
+                        onVisibleChange: (visible) => setPreviewOpen(visible),
+                        afterOpenChange: (visible) => !visible && setPreviewImage(''),
+                    }}
+                    src={previewImage}
+                />
+            )}
 
-                    <div>
-                        <img src={currentImage?currentImage.src:''} style={{ height: "40vh", width: "30vw" }} />
-                    </div>
-
-                   
-                    <div 
-              style={{
-                height: '20rem',
-                width:'12rem',
-                overflow: 'auto',
-                // overflow:'hidden', 
-                scrollbarWidth: 'none'
-              }}>
-                 <Flex vertical gap={'large'}>
-                        {srcList.map((item, i) => (
-                            <Card 
-                            key={i} 
-                            onClick={() => { handleCurrentImage(item) }} 
-                            style={{
-                                border: currentImage && currentImage.name && currentImage.name.includes('_') && i==currentImage.name.split('_')[1]?'1px solid green':''}}>
-                                <Flex justify='space-around'>
-                                <p style={{margin:'0px'}}>{item.name}</p> 
-                                {/* <div
-                                 onClick={() => { handleDelete(item) }}
-                                 >
-                                <img src={del} alt="My Icon" style={{ width: '30px', height: '30px' }} />
-                                </div> */}
-                                </Flex>
-                            </Card>
-                        ))}
-                            </Flex>
-
-                        </div>
-                
-                </Flex>
-
-
-                <Flex vertical gap={40} align='center'>
-
-
-
-                    <Button onClick={uploadFiles}>Save</Button>
-
-                    <Upload.Dragger {...draggerProps}>
-                        <div>
-                            <p className="ant-upload-drag-icon">
-                                {/* <UploadOutlined /> */}
-                            </p>
-                            <p className="ant-upload-text">
-                                Click or drag file to this area to upload
-                            </p>
-                            <Button
-                                className="button-submit avatar-button-style"
-                                style={{
-                                    color: "white",
-                                    backgroundColor: "rgb(29, 155, 240)",
-                                }}
-                            >
-                                <span style={{ color: "white", fontWeight: "bolder" }}>
-                                    Select from Computer
-                                </span>
-                            </Button>
-                        </div>
-                    </Upload.Dragger>
-                </Flex>
-            </Context.Provider>
         </div>
     )
 }
 
 export default ImageUpload
+
--- src/utils/locale.js
+++ src/utils/locale.js
@@ -1,5 +1,9 @@
 export const MESSAGES = {
     FETCH_SERVICES_ERROR : 'Unable t fetch Services',
     UPDATE_SERVICES_ERROR : 'Unable to update Services',
-    UPDATE_SERVICES_SUCCESS: 'Services Updated Successfully'
+    UPDATE_SERVICES_SUCCESS: 'Services Updated Successfully',
+    DELETE_PLANT_IMAGES: 'Images deleted Successfully',
+    UPLOAD_PLANT_IMAGES: 'Images Uploaded Successfully',
+    UPLOAD_IMAGE_ERROR: 'Unable to Upload Image',
+    FETCH_IMAGE_ERROR: 'Unable to Fetch Image'
 }

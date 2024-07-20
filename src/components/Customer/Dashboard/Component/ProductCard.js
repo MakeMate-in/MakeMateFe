00b67c843b003fec54ee73b773aa229fb8889475
VAIBHAV GARG <63347618+Vaibhav2801@users.noreply.github.com>
2024-07-20 22:56:25 +0530

    Home page

--- src/components/Customer/Dashboard/Component/ProductCard.js
+++ src/components/Customer/Dashboard/Component/ProductCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Carousel, Flex, Typography, Button, Rate } from 'antd';
+import { Carousel, Flex, Typography, Button, Rate, Badge, Tag } from 'antd';
 import './ProductCard.css'
 import { bg1 } from '../../../../utils/colorGradient';
 
@@ -42,50 +42,58 @@ const ProductCard = (props) => {
         {props.data.images && props.data.images.map((image, index) => {
           return (
             <div key={index} className='bg-gray-500 rounded-2xl object-cover aspect-square container-fluid'>
-              <img src={image} alt={`Slide ${index}`} className='rounded-2xl object-cover aspect-squuare' style={{height:"100%"}} />
+              <img src={image} alt={`Slide ${index}`} className='rounded-2xl object-cover aspect-squuare' style={{ height: "100%" }} />
             </div>
           )
         }
         )}
       </Carousel>
-             <Typography className="text-2xl font-bold mb-2">{props.data.product_name}</Typography>
-             <Typography className="text-2xl font-bold mb-2">{props.data.company_data.company_name}</Typography>
-             <Typography >{props.data.company_data.description}</Typography>
-             <Typography >{props.data.company_data.current_projects_number}</Typography>
-             <Typography >{props.data.company_data.experience}</Typography>
-             <Typography >{props.data.infrastructuredetails.plant_area}</Typography>
-             {
-              props.data.Certificstes && props.data.Certificstes.map((item) => {
-              return  (<Typography >{item.fileName}</Typography>)
-              })
-             }
-             {
-              props.data.services.map((item) => {
-                if(item.services.service_type=="Inhouse"){
-                  return (<Typography >{item.services.service_name}</Typography>)
-                }
-              })
-             }
-              {
-              props.data.services.map((item) => {
-                if(item.services.service_type=="Outsourced"){
-                  return (<Typography >{item.services.service_name}</Typography>)
-                }
-              })
-             }
-             {
-              props.data.machine_details.map((item) => {
-                  return (<Typography >{item.machine_type}</Typography>)
-                
-              })
-             }
-             <Rate/>
-             <hr/>
-             <Flex vertical gap={20}>
-             <Button type='primary'  style={{ fontSize: '18px', fontWeight: '600', height: '40px', display: 'flex', alignItems: 'center', background: bg1}}>Connect</Button>
-                 <Button type='primary' 
-             style={{ fontSize: '18px', fontWeight: '600', height: '40px', display: 'flex', alignItems: 'center', background: bg1}}>Feedback</Button>
-             </Flex>
+      {
+        props.data.Certificstes && props.data.Certificstes.map((item) => {
+          return (<Badge.Ribbon text={item.fileName} />)
+        })
+      }
+      <Flex vertical gap={5}>
+      <Typography style={{ fontSize: '24px', fontWeight: '400' }}>{props.data.product_name}</Typography>
+      <Flex gap={10}>
+        <Typography >{props.data.company_data.company_name}, </Typography>
+        <Typography >{props.data.company_data.description}</Typography>
+      </Flex>
+      <Rate />
+      <br />
+      <Typography >Experience: {props.data.company_data.experience}</Typography>
+      <Typography >Plant Area: {props.data.infrastructuredetails.plant_area}</Typography>
+
+      {
+        props.data.services.map((item) => {
+          if (item.services.service_type == "Inhouse") {
+            return (<Typography >{item.services.service_name}</Typography>)
+          }
+        })
+      }
+      {
+        props.data.services.map((item) => {
+          if (item.services.service_type == "Outsourced") {
+            return (<Typography >{item.services.service_name}</Typography>)
+          }
+        })
+      }
+      <Flex>
+        {
+
+          props.data.machine_details.map((item) => {
+            return (<Tag style={{ background: 'purple', color: 'white' }} >{item.machine_type}</Tag>)
+
+          })
+        }
+      </Flex>
+
+      <Flex vertical gap={20}>
+        <Button type='primary' style={{ fontSize: '18px', fontWeight: '600', height: '40px', display: 'flex', alignItems: 'center', background: bg1 }}>Connect</Button>
+        <Button type='primary'
+          style={{ fontSize: '18px', fontWeight: '600', height: '40px', display: 'flex', alignItems: 'center', background: bg1 }}>Feedback</Button>
+      </Flex>
+      </Flex>
     </div>
   )
 }
